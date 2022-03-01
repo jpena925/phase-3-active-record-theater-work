1e6dc80e1eb30c5cf575c24519f54fc9a5a62b0c
Jack Pena <penajackc@gmail.com>
2022-03-01 09:53:32 -0700

    seeded

---
+++ db/seeds.rb
@@ -0,0 +1,18 @@
+puts "starting seeding"
+
+Audition.destroy_all
+Role.destroy_all
+
+10.times do
+    Role.create(character_name: Faker::TvShows::SouthPark.character)
+end
+
+20.times do 
+    Audition.create(actor: Faker::Name.name, 
+    location: Faker::Address.city, 
+    phone: Faker::PhoneNumber.cell_phone, 
+    hired: Faker::Boolean.boolean, 
+    role_id: Role.all.sample.id)
+end
+
+puts "finished seeding!"
